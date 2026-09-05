5eeab7e08ab37a712e5028c7197df7a9cf97916e
Manav948 <valanimanav61@gmail.com>
2026-09-05 23:29:51 +0530

    implement multi thread real time chat feature

--- client/client.c
+++ client/client.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #ifdef _WIN32
     #include <winsock2.h>
     #include <ws2tcpip.h>
+    #include <windows.h>
     #pragma comment(lib, "ws2_32.lib")
     #define close_socket closesocket
 #else
@@ -13,6 +15,7 @@
     #include <sys/socket.h>
     #include <netinet/in.h>
     #include <arpa/inet.h>
+    #include <pthread.h>
     typedef int SOCKET;
     #define INVALID_SOCKET -1
     #define SOCKET_ERROR -1
@@ -23,24 +26,70 @@
 #define port 8080
 #define BUFFER_SIZE 1024
 
+// Dedicated Background Thread: Listens continuously for incoming network messages
+#ifdef _WIN32
+DWORD WINAPI receive_thread(LPVOID arg) {
+    SOCKET sock_fd = *(SOCKET*)arg;
+    char buffer[BUFFER_SIZE];
+
+    while (1) {
+        int bytes_received = recv(sock_fd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received > 0) {
+            buffer[bytes_received] = '\0';
+            // Print incoming broadcast message immediately!
+            printf("%s", buffer);
+            fflush(stdout);
+        } else if (bytes_received == 0) {
+            printf("\n[CLIENT] Server closed connection.\n");
+            exit(0);
+        } else {
+            perror("\n[CLIENT] Connection error / lost");
+            exit(1);
+        }
+    }
+    return 0;
+}
+#else
+void* receive_thread(void* arg) {
+    SOCKET sock_fd = *(SOCKET*)arg;
+    char buffer[BUFFER_SIZE];
+
+    while (1) {
+        int bytes_received = recv(sock_fd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received > 0) {
+            buffer[bytes_received] = '\0';
+            printf("%s", buffer);
+            fflush(stdout);
+        } else if (bytes_received == 0) {
+            printf("\n[CLIENT] Server closed connection.\n");
+            exit(0);
+        } else {
+            perror("\n[CLIENT] Connection error / lost");
+            exit(1);
+        }
+    }
+    return NULL;
+}
+#endif
+
 int main(void) {
-    #ifdef _WIN32
+#ifdef _WIN32
     WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
-        printf("Failed to initialize winsock.\n");
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
+        printf("Failed to initialize Winsock.\n");
         return 1;
     }
-    #endif
-    //  create socket
-    SOCKET sock_fd = socket(AF_INET, SOCK_STREAM , 0);
-    if(sock_fd == INVALID_SOCKET) {
-        perror("socket creation failed");
+#endif
+
+    // 1. Create socket
+    SOCKET sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd == INVALID_SOCKET) {
+        perror("Socket creation failed");
         return 1;
     }
-    printf("Socket created successfully \n");
+    printf("[CLIENT] Socket created successfully\n");
 
-    // specify server address and port
-
+    // 2. Specify server address
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
@@ -48,66 +97,62 @@ int main(void) {
     server_addr.sin_addr.s_addr = inet_addr(server_ip);
 
     if (server_addr.sin_addr.s_addr == INADDR_NONE) {
-        printf("[CLIENT] Invalid IP address / Address not supported\n");
+        printf("[CLIENT] Invalid IP address\n");
         close_socket(sock_fd);
         return 1;
     }
     printf("[CLIENT] Connecting to server at %s:%d...\n", server_ip, port);
 
-    // connect to server (Triggers TCP 3-Way Handshake: SYN -> SYN-ACK -> ACK)
-    if(connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR){
-        perror("connect failed");
+    // 3. Connect to server
+    if (connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        perror("Connect failed");
         close_socket(sock_fd);
         return 1;
     }
     printf("[CLIENT] Successfully connected to server!\n");
 
-    // receive response from server
+    // 4. Start Background Receive Thread so incoming messages display INSTANTLY
+#ifdef _WIN32
+    HANDLE recv_thread = CreateThread(NULL, 0, receive_thread, &sock_fd, 0, NULL);
+    if (recv_thread == NULL) {
+        printf("[CLIENT] Failed to create receive thread.\n");
+        close_socket(sock_fd);
+        return 1;
+    }
+#else
+    pthread_t recv_thread;
+    if (pthread_create(&recv_thread, NULL, receive_thread, &sock_fd) != 0) {
+        printf("[CLIENT] Failed to create receive thread.\n");
+        close_socket(sock_fd);
+        return 1;
+    }
+#endif
+
+    // 5. Main Thread Loop: Handles user keyboard input & sending
     char buffer[BUFFER_SIZE];
-    int bytes_received = recv(sock_fd, buffer, sizeof(buffer) - 1, 0);
+    while (1) {
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            break;
+        }
 
-    if (bytes_received > 0) {
-        buffer[bytes_received] = '\0';
-        printf("[CLIENT] Received reply from server: \"%s\" (%d bytes)\n", buffer, bytes_received);
-    }
-    // iterative loop for multiple clints
-    while(1) {
-        printf("You >");
-        if(fgets(buffer, sizeof(buffer), stdin) == NULL) {
+        if (strncmp(buffer, "/quit", 5) == 0) {
+            printf("[CLIENT] Disconnecting...\n");
             break;
         }
-        // check for disconnect command
-        if(strncmp(buffer, "/quit", 5) == 0) {
-            printf("[CLIENT] DisConnect...\n");
-            break;
-        }
-        int bytes_sent = send(sock_fd, buffer, strlen(buffer), 0);
-        if(bytes_sent == SOCKET_ERROR) {
-            perror("send failed");
-            break;
-        }
-        bytes_received = recv(sock_fd, buffer, sizeof(buffer) - 1 , 0);
 
-        if (bytes_received > 0) {
-            buffer[bytes_received] = '\0';
-            printf("[CLIENT] Received reply from server: \"%s\" (%d bytes)\n", buffer, bytes_received);
-        } else if (bytes_received == 0) {
-            printf("[CLIENT] Server closed the connection\n");
-            break;
-        } else {
-            perror("recv failed");
+        printf("[YOU]: %s", buffer);
+
+        // Send message to server (Receive Thread will catch any responses/broadcasts asynchronously!)
+        int bytes_sent = send(sock_fd, buffer, (int)strlen(buffer), 0);
+        if (bytes_sent == SOCKET_ERROR) {
+            perror("[ERROR] Send failed");
             break;
         }
     }
-    
 
-    // close socket
-    printf("[CLIENT] Closing the socket\n");
     close_socket(sock_fd);
-
-    #ifdef _WIN32
-        WSACleanup();
-    #endif
-
+#ifdef _WIN32
+    WSACleanup();
+#endif
     return 0;
 }
--- server/server.c
+++ server/server.c
@@ -22,9 +22,56 @@ typedef int SOCKET;
 #endif
 
 #define port 8080
-#define backlog 5
+#define backlog 10
 #define buffer_size 1024
 #define max_client 30
+#define username_len 32
+
+// Define structure to hold client information
+typedef struct
+{
+    SOCKET socket_fd;
+    char username[username_len];
+    int is_registered;
+} Client;
+
+Client clients[max_client];
+
+// Trim newline characters from string
+void trim_newline(char *str)
+{
+    size_t len = strlen(str);
+    while (len > 0 && (str[len - 1] == '\n' || str[len - 1] == '\r'))
+    {
+        str[len - 1] = '\0';
+        len--;
+    }
+}
+
+// Check if username is already taken
+int userNameTaken(const char *username)
+{
+    for (int i = 0; i < max_client; i++)
+    {
+        if (clients[i].is_registered && strcmp(clients[i].username, username) == 0)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Helper: Broadcast a message to all registered clients except sender (if sender_fd != 0)
+void broadcast_message(const char *msg, SOCKET sender_fd)
+{
+    for (int i = 0; i < max_client; i++)
+    {
+        if (clients[i].is_registered && clients[i].socket_fd > 0 && clients[i].socket_fd != sender_fd)
+        {
+            send(clients[i].socket_fd, msg, (int)strlen(msg), 0);
+        }
+    }
+}
 
 int main(void)
 {
@@ -32,32 +79,32 @@ int main(void)
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
-        printf("Failed to initialize winsock. \n");
+        printf("[ERROR] Failed to initialize Winsock.\n");
         return 1;
     }
 #endif
 
-    // track active client
-    SOCKET client_sockets[max_client];
+    // Initialize client tracker
     for (int i = 0; i < max_client; i++)
     {
-        client_sockets[i] = 0;
+        clients[i].socket_fd = 0;
+        clients[i].username[0] = '\0';
+        clients[i].is_registered = 0;
     }
 
-    // create listening socket
+    // 1. Create Listening Socket
     SOCKET server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == INVALID_SOCKET)
     {
-        perror("Socket create failed");
+        perror("[ERROR] Socket creation failed");
         return 1;
     }
 
-    // re-ues socket for multiple client connection
     int op = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char *)&op, sizeof(op));
-    printf("Socket created successfully (FD: %d)\n", (int)server_fd);
+    printf("[INIT] Listening socket created successfully (FD: %d)\n", (int)server_fd);
 
-    // bind socket to ip and port
+    // 2. Bind Socket
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
@@ -66,22 +113,24 @@ int main(void)
 
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
     {
-        perror("Bind failed");
+        perror("[ERROR] Bind failed");
         close_socket(server_fd);
         return 1;
     }
-    printf("Socket bound to port %d\n", port);
+    printf("[INIT] Socket bound to 0.0.0.0:%d\n", port);
 
-    // listen for incoming connection
+    // 3. Listen
     if (listen(server_fd, backlog) == SOCKET_ERROR)
     {
-        perror("Listen failed");
+        perror("[ERROR] Listen failed");
         close_socket(server_fd);
         return 1;
     }
-    printf("Server is listening for incoming connections on port %d (backlog: %d)...\n", port, backlog);
+ 
+    printf("   REAL-TIME TCP CHAT SERVER RUNNING (PORT: %d)\n", port);
+    printf("   Max Capacity: %d clients | Backlog: %d\n", max_client, backlog);
 
-    // accept connection
+
     fd_set readfds;
     char buffer[buffer_size];
 
@@ -93,7 +142,7 @@ int main(void)
 
         for (int i = 0; i < max_client; i++)
         {
-            SOCKET sd = client_sockets[i];
+            SOCKET sd = clients[i].socket_fd;
             if (sd > 0)
             {
                 FD_SET(sd, &readfds);
@@ -103,16 +152,15 @@ int main(void)
                 max_fd = sd;
             }
         }
-        // Wait for activity on one of the sockets
+
         int activity = select((int)(max_fd + 1), &readfds, NULL, NULL, NULL);
         if (activity < 0)
         {
-            perror("select error");
+            perror("[ERROR] Select error");
             break;
         }
 
-        //  Incoming New Connection on Listening Socket
-
+        // Incoming New Connection
         if (FD_ISSET(server_fd, &readfds))
         {
             struct sockaddr_in client_addr;
@@ -120,65 +168,121 @@ int main(void)
             SOCKET new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
             if (new_socket != INVALID_SOCKET)
             {
-                char *client_ip = inet_ntoa(client_addr.sin_addr);
-                printf("[SERVER] New connection! IP: %s, Port: %d (Socket FD: %d)\n", client_ip,
-                       ntohs(client_addr.sin_port), (int)new_socket);
-
-                const char *welcomeMessage = "Welcome to the server!";
-                send(new_socket, welcomeMessage, (int)strlen(welcomeMessage), 0);
-
-                // add new socket into array of sockets
                 int added = 0;
                 for (int i = 0; i < max_client; i++)
                 {
-                    if (client_sockets[i] == 0)
+                    if (clients[i].socket_fd == 0)
                     {
-                        client_sockets[i] = new_socket;
+                        clients[i].socket_fd = new_socket;
+                        clients[i].is_registered = 0;
+                        clients[i].username[0] = '\0';
                         added = 1;
-                        printf("[SERVER] Added client to slot %d\n", i);
+
+                        char *client_ip = inet_ntoa(client_addr.sin_addr);
+                        printf("[CONNECT] New connection from %s:%d (Socket FD: %d)\n",
+                               client_ip, ntohs(client_addr.sin_port), (int)new_socket);
+
+                        const char *welcomeMessage = "[SERVER] Welcome! Please enter your username: ";
+                        send(new_socket, welcomeMessage, (int)strlen(welcomeMessage), 0);
                         break;
                     }
                 }
                 if (!added)
                 {
-                    printf("[SERVER] Server full! Rejecting client.\n");
+                    printf("[WARN] Server full! Connection rejected (FD: %d)\n", (int)new_socket);
+                    const char *full_msg = "[SERVER] Server full! Disconnecting.\n";
+                    send(new_socket, full_msg, (int)strlen(full_msg), 0);
                     close_socket(new_socket);
                 }
             }
         }
-        // IO operation on some other socket
-        for(int i = 0; i < max_client ; i++) {
-            SOCKET sd = client_sockets[i];
-            if(sd > 0 && FD_ISSET(sd, &readfds)) {
-                int bytes_received = recv(sd, buffer, sizeof(buffer) - 1, 0);
-                
-                if(bytes_received > 0) {
-                    buffer[bytes_received] = '\0';
-                     printf("[CLIENT FD %d]: %s", (int)sd, buffer);
 
-                    char replay[buffer_size];
-                    snprintf(replay, sizeof(replay), "Server received: %s", buffer);
-                    send(sd, replay, (int)strlen(replay), 0);
-                }else if(bytes_received == 0) {
-                    //  client dis-connected
-                    struct sockaddr_in client_addr;
-                    socklen_t client_len = sizeof(client_addr);
-                    getpeername(sd, (struct sockaddr *)&client_addr, &client_len);
-                    printf("[SERVER] Client disconnected! IP: %s, Port: %d (Socket FD: %d)\n", inet_ntoa(client_addr.sin_addr),
-                           ntohs(client_addr.sin_port), (int)sd);
+        //  I/O Activity on Client Sockets
+        for (int i = 0; i < max_client; i++)
+        {
+            SOCKET sd = clients[i].socket_fd;
+            if (sd > 0 && FD_ISSET(sd, &readfds))
+            {
+                int bytes_received = recv(sd, buffer, sizeof(buffer) - 1, 0);
+
+                if (bytes_received > 0)
+                {
+                    buffer[bytes_received] = '\0';
+                    trim_newline(buffer);
+
+                    // Client Registration Phase
+                    if (!clients[i].is_registered)
+                    {
+                        if (strlen(buffer) == 0)
+                        {
+                            const char *err = "[SERVER] Username cannot be empty. Enter username: ";
+                            send(sd, err, (int)strlen(err), 0);
+                        }
+                        else if (userNameTaken(buffer))
+                        {
+                            const char *err = "[SERVER] Username already taken! Choose another: ";
+                            send(sd, err, (int)strlen(err), 0);
+                        }
+                        else
+                        {
+                            strncpy(clients[i].username, buffer, username_len - 1);
+                            clients[i].username[username_len - 1] = '\0';
+                            clients[i].is_registered = 1;
+
+                            printf("[REGISTER] FD %d registered as '%s'\n", (int)sd, clients[i].username);
+
+                            // Send confirmation to registered user
+                            char welcome_msg[buffer_size];
+                            snprintf(welcome_msg, sizeof(welcome_msg),
+                                     "[SERVER] Welcome, %s! Type your message and hit Enter.\n", clients[i].username);
+                            send(sd, welcome_msg, (int)strlen(welcome_msg), 0);
+
+                            // Broadcast join notification to all OTHER connected users!
+                            char join_notification[buffer_size];
+                            snprintf(join_notification, sizeof(join_notification),
+                                     "[SERVER] *** %s joined the chat! ***\n", clients[i].username);
+                            broadcast_message(join_notification, sd);
+                        }
+                    }
+                    // Chat Broadcast Phase
+                    else
+                    {
+                        printf("[CHAT] %s (FD %d): %s\n", clients[i].username, (int)sd, buffer);
+
+                        // Broadcast chat message to ALL OTHER clients!
+                        char broadcast_msg[buffer_size];
+                        snprintf(broadcast_msg, sizeof(broadcast_msg), "[%s]: %s\n", clients[i].username, buffer);
+                        broadcast_message(broadcast_msg, sd);
+                    }
+                }
+                else
+                {
+                    // Client Disconnection Handling
+                    if (clients[i].is_registered)
+                    {
+                        printf("[DISCONNECT] User '%s' (FD %d) disconnected.\n", clients[i].username, (int)sd);
+
+                        // Broadcast leave notification to remaining users
+                        char leave_notification[buffer_size];
+                        snprintf(leave_notification, sizeof(leave_notification),
+                                 "[SERVER] *** %s left the chat. ***\n", clients[i].username);
+                        broadcast_message(leave_notification, sd);
+                    }
+                    else
+                    {
+                        printf("[DISCONNECT] Unregistered client (FD %d) disconnected.\n", (int)sd);
+                    }
+
                     close_socket(sd);
-                    client_sockets[i] = 0;
-                }else {
-                    perror("recv failed");
-                    close_socket(sd);
-                    client_sockets[i] = 0;
+                    clients[i].socket_fd = 0;
+                    clients[i].is_registered = 0;
+                    clients[i].username[0] = '\0';
                 }
             }
         }
     }
-    // close connection
-    printf("Closing the connection\n");
-    close_socket(server_fd);
+
+    printf("[SHUTDOWN] Server closing...\n");
     close_socket(server_fd);
 
 #ifdef _WIN32
